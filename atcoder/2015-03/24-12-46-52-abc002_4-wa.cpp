030d3a7277f0350db05066a24b32b9785c53899e
vain0x <vainzerox@gmail.com>
2015-03-24 12:46:52 +0900

    https://beta.atcoder.jp/contests/abc002/submissions/366611

---
+++ atcoder/2015-03/24-12-46-52-abc002_4-wa.cpp
@@ -0,0 +1,53 @@
+//abc001.contest.atcoder.jp/tasks/abc002_4
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <array>
+#include <map>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+using N = unsigned int;
+#define repeat(_I, _N) for(N _I = 0; _I < _N; ++_I)
+#define all(_X) (_X).begin(), (_X).end()
+
+int main()
+{
+	N count_members, count_rels;
+	vector<pair<N, N>> rels;
+
+	cin >> count_members >> count_rels;
+	repeat(i, count_rels) {
+		N x, y;
+		cin >> x >> y;
+		rels.emplace_back(make_pair(x, y));
+		rels.emplace_back(make_pair(y, x));
+	}
+
+	N first_person = 1;
+	N next_person =0; //次のグループの起点
+	N max_count = 1;
+	for(;;) {
+		set<N> g = { first_person };
+		for ( N i = 1; i <= count_members; ++i ) {
+
+			//現時点の各グループメンバ j について i と j が知り合い、であればiは構成員になる
+			bool able = all_of(all(g), [&](N j) {
+				return i == j || find(all(rels), make_pair(i, j)) != rels.end();
+			});
+			
+			if ( able ) {
+				g.insert(i);
+			} else {
+				if ( i > first_person && next_person == 0 ) { next_person = i; }
+			}
+		}
+
+		if ( g.size() > max_count ) max_count = g.size();
+		if ( next_person == 0 ) break;
+		first_person = next_person; next_person = 0;
+	}
+
+	printf("%d\n", max_count);
+}
