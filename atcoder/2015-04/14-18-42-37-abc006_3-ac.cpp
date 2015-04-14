dc3e2ab84e2459270f88959d944fe382acc1c8b7
vain0x <vainzerox@gmail.com>
2015-04-14 18:42:37 +0900

    https://beta.atcoder.jp/contests/abc006/submissions/386142

---
+++ atcoder/2015-04/14-18-42-37-abc006_3-ac.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+#define repi(_I, _Init, _N) for (int _I = (_Init); (_I) < (_N); ++(_I))
+#define rep(_I, _N) repi(_I, 0, _N)
+
+void mymain()
+{
+	int n, m;
+	cin >> n >> m;
+	rep(x, n+1) {
+		int y = 4 * n - m - 2 * x;
+		int z = n - (x + y);
+		if ( x >= 0 && y >= 0 && z >= 0 ) {
+			cout << x << ' ' << y << ' ' << z << endl;
+			return;
+		}
+	}
+	cout << "-1 -1 -1" << endl;
+}
+
+int main() { std::ios::sync_with_stdio(false); std::cin.tie(0); mymain(); return 0; }
