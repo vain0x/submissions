a71d3b87d042abbf08116d6c9454490ab0e987e3
vain0x <vainzerox@gmail.com>
2015-05-10 00:47:44 +0900

    https://beta.atcoder.jp/contests/abc023/submissions/402556

---
+++ atcoder/2015-05/10-00-47-44-abc023_d-wa.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+using ull = unsigned long long;
+#define rep(_I, _N) for(unsigned int _I = 0; (_I) < (_N); ++ (_I))
+int main() { void mymain(); std::ios::sync_with_stdio(false); std::cin.tie(0); mymain(); return 0; }
+
+ull n;
+ull hs[100000 + 1];
+ull ss[100000 + 1];
+ull us[100000 + 1];
+
+void mymain() {
+	cin >> n;
+
+	ull maxh = 0;
+	rep(i, n) {
+		cin >> hs[i] >> ss[i];
+		maxh = max(maxh, hs[i]);
+	}
+
+	ull low = maxh; //達成不可能な最大数
+	ull high = 3000000000ull + 1; //達成可能な最小数
+	rep(_, 30) {
+		ull m = (high + low) / 2;
+
+		memset(us, 0, (n + 1) * sizeof(ull));
+		rep(i, n) {
+			ull u = min(n, (m - hs[i]) / ss[i]); //t[i]はu以下でなければいけない
+			us[u] ++;
+		}
+
+		bool ok = true;
+		ull u = 0;
+		rep(i, n) {
+			u += us[i];
+			if ( u > i + 1 ) { ok = false; break; }
+		}
+		if ( ok ) {
+			high = m;
+		} else {
+			low = m;
+		}
+	}
+
+	cout << high << endl;
+}
