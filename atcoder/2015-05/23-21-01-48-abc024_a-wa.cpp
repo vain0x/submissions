660d3384e0fbeeb5a8682a38324687acaf84115b
vain0x <vainzerox@gmail.com>
2015-05-23 21:01:48 +0900

    Add https://atcoder.jp/contests/abc024/submissions/412884

---
+++ atcoder/2015-05/23-21-01-48-abc024_a-wa.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE //POJ
+# include <random>
+# include <array>
+# define mkt make_tuple
+# define empb emplace_back
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#endif
+using namespace std;
+typedef unsigned int uint; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define mkp make_pair
+#define all(_X) (_X).begin(), (_X).end()
+int main() { void mymain(); mymain(); return 0; }
+
+
+void mymain() {
+	int a, b, c, k, s, t;
+	cin >> a >> b >> c >> k >> s >> t;
+	int x = a * s + t * b;
+	if ( s + t >= k ) x -= c * (s + t);
+	cout << x;
+}
