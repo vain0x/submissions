f6538214ca961a090467a3784f90abf2e7b2405e
vain0x <vainzerox@gmail.com>
2015-06-27 21:03:47 +0900

    Add https://atcoder.jp/contests/abc025/submissions/432350

---
+++ atcoder/2015-06/27-21-03-47-abc025_a-ac.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE //POJ
+# include <complex>
+# include <random>
+# include <array>
+# define mkt make_tuple
+# define empb emplace_back
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#endif
+using namespace std;
+typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define mkp make_pair
+#define all(_X) (_X).begin(), (_X).end()
+inline int scani() { int n; scanf("%d", &n); return n; }
+
+
+
+signed main() {
+	int n;
+	string s;
+	cin >> s >> n;
+	--n;
+
+	cout << s[n / 5] << s[n % 5] << endl;
+	return 0;
+}
