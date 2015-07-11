494c14444d098f53566dd8a739b0504f93a76ba4
vain0x <vainzerox@gmail.com>
2015-07-11 21:00:59 +0900

    Add https://atcoder.jp/contests/abc026/submissions/443903

---
+++ atcoder/2015-07/11-21-00-59-abc026_a-ac.cpp
@@ -0,0 +1,43 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE //POJ
+# include <complex>
+# include <random>
+# include <array>
+# define mkt make_tuple
+# define empb emplace_back
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#else
+# define debug if (false)
+#endif
+using namespace std;
+typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define mkp make_pair
+#define all(_X) (_X).begin(), (_X).end()
+
+
+signed main()
+{
+	int n;
+	cin >> n;
+	cout << (n / 2) * (n/2) << endl;
+
+
+	return 0;
+}
