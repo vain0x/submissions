c9a6b6d8896fbd7e825a7d79e18bb970da68aab7
vain0x <vainzerox@gmail.com>
2015-07-19 16:30:23 +0900

    https://beta.atcoder.jp/contests/abc005/submissions/451599

---
+++ atcoder/2015-07/19-16-30-23-abc005_1-ac.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	cout << (y / x) << endl;
+	return 0;
+}
