7a06076911af4a6a427566fc041c04af0d0939f9
vain0x <vainzerox@gmail.com>
2015-07-25 22:55:35 +0900

    Add https://atcoder.jp/contests/arc042/submissions/455357

---
+++ atcoder/2015-07/25-22-55-35-arc042_c-ac.cpp
@@ -0,0 +1,65 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE //POJ
+# include <complex>
+# include <random>
+# include <array>
+# define mkt make_tuple
+# define empb emplace_back
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#endif
+using namespace std;
+typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define mkp make_pair
+#define mkt make_tuple
+#define all(_X) (_X).begin(), (_X).end()
+inline int scani() { int n; scanf("%d", &n); return n; }
+
+using pii = pair<int,int>;
+int n, p;
+vector<pii> xs;
+
+signed main()
+{
+	cin >> n >> p;
+	
+	xs.resize(n);
+	rep(i, n)
+	{
+		cin >> xs[i].first >> xs[i].second;
+	}
+	sort(all(xs));
+
+	vector<int> dp(p + xs.back().first + 1, 0);
+	int result = 0;
+	for(int j = n - 1; j >= 0;--j) {
+		int i = j + 1;
+		if ( i < n ) {
+			for ( int q = p; q >= 0; --q ) {
+				if ( q < xs[i].first ) continue;
+				dp[q] = max(dp[q],
+					dp[q - xs[i].first] + xs[i].second);
+			}
+		}
+		result = max(result, dp[p] + xs[j].second);
+	}
+	cout << result << endl;
+
+	return 0;
+}
