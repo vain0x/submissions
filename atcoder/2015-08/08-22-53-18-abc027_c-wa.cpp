5a1f6d770e78bdb434d336b268086323afd80135
vain0x <vainzerox@gmail.com>
2015-08-08 22:53:18 +0900

    Add https://atcoder.jp/contests/abc027/submissions/467119

---
+++ atcoder/2015-08/08-22-53-18-abc027_c-wa.cpp
@@ -0,0 +1,53 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE
+# include <complex>
+# include <random>
+# include <array>
+# define mkt make_tuple
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#else
+# define ifdebug if (false)
+# define echo ((void)0)
+#endif
+using namespace std;
+typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define all(_X) (_X).begin(), (_X).end()
+#define mkp make_pair
+inline int scani() { int n; scanf(" %d", &n); return n; }
+
+ull n;
+
+bool solve() {
+	bool w = false;
+	for(ull k = n/2, l = n+1; k > 1; ) {
+		k = (k + 1) / 2;
+		if ( k * 2 >= l ) --k;
+		l = (l + 1) / 2;
+		w = !w;
+	}
+	return w;
+}
+
+signed main()
+{
+	cin >> n;
+	cout << (solve() ? "Takahashi" : "Aoki") << endl;
+	return 0;
+}
