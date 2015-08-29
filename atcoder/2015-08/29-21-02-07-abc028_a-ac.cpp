bea743e547a22697e893fd6f670c38ab0d398d34
vain0x <vainzerox@gmail.com>
2015-08-29 21:02:07 +0900

    https://beta.atcoder.jp/contests/abc028/submissions/479205

---
+++ atcoder/2015-08/29-21-02-07-abc028_a-ac.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <functional>
+#include <set>
+#include <ctime>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#ifndef ONLINE_JUDGE
+# include <complex>
+# include <random>
+# include <array>
+# include <unordered_map>
+# define mkt make_tuple
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#else
+# define ifdebug if (false)
+# define echo ((void)0)
+#endif
+using namespace std;
+typedef unsigned int uint; typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define all(_X) (_X).begin(), (_X).end()
+#define mkp make_pair
+inline int scani() { int n; scanf(" %d", &n); return n; }
+
+char const* solve(int n)
+{
+	if ( n == 100 ) {
+		return "Perfect";
+	} else if ( n >= 90 ) {
+		return "Great";
+	} else if ( n >= 60 ) {
+		return "Good";
+	} else {
+		return "Bad";
+	}
+
+}
+
+signed main()
+{
+	int n;
+	cin >> n;
+	cout << solve(n) << endl;
+	return 0;
+}
