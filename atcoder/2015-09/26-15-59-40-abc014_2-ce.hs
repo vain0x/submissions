c68baeded94322f1cbc4efedb49e24437d557fbf
vain0x <vainzerox@gmail.com>
2015-09-26 15:59:40 +0900

    https://beta.atcoder.jp/contests/abc014/submissions/507694

---
+++ atcoder/2015-09/26-15-59-40-abc014_2-ce.hs
@@ -0,0 +1,10 @@
+import Data.Bits
+
+readLnIntList :: IO [Int]
+readLnIntList = fmap (fmap read . words) getLine
+
+main = do
+    [n, x] <- readLnIntList
+    xs <- readLnIntList
+    putStrLn $ show $
+        sum [if testBit x i then xs !! i else 0 | i <- [0..(n - 1)]] }
