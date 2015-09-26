1e646c15ea842944740834210f3c1a7a55417831
vain0x <vainzerox@gmail.com>
2015-09-26 21:04:17 +0900

    https://beta.atcoder.jp/contests/code-festival-2015-quala/submissions/508928

---
+++ atcoder/2015-09/26-21-04-17-codefestival_2015_quala_b-ac.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <cmath>
+#ifndef ONLINE_JUDGE
+# include <complex>
+# include <random>
+# include <array>
+# include <unordered_map>
+# define mkt make_tuple
+#endif
+#ifdef _LOCAL
+# include "for_local.h"
+#else
+# define assert(...) ((void)0)
+# define ifdebug if (false)
+# define echo(...) ((void)0)
+#endif
+using namespace std;
+typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define all(_X) (_X).begin(), (_X).end()
+
+string s;
+
+int main()
+{
+	int n;
+	cin >> n;
+	ll s = 0;
+	rep(i, n)
+	{
+		ll a;
+		cin >> a;
+		s = s * 2 + a;
+	}
+	cout << s << endl;
+
+	return 0;
+}
