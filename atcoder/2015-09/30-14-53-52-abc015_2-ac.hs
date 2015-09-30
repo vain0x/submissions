ba9970ab5d2139f3fefe9ec3e002d0fe81aad7e1
vain0x <vainzerox@gmail.com>
2015-09-30 14:53:52 +0900

    Add https://atcoder.jp/contests/abc015/submissions/517425

---
+++ atcoder/2015-09/30-14-53-52-abc015_2-ac.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Control.Applicative ((<$>), (<*>))
+
+readLines :: (Read a) => Int -> IO [a]
+readLines n =
+    (concatMap $ fmap read . words) <$> replicateM n getLine
+
+main :: IO ()
+main = do
+    [n] <- readLines 1 :: IO [Int]
+    xs <- readLines 1 :: IO [Int]
+    let ys = filter ((/=) 0) xs
+    let m = length ys
+    print $ (sum ys + (m - 1)) `div` m
