01fdfe7cb6243e3707d91ac243877f2a8609c3c2
vain0x <vainzerox@gmail.com>
2015-10-10 21:04:34 +0900

    https://beta.atcoder.jp/contests/arc045/submissions/523343

---
+++ atcoder/2015-10/10-21-04-34-arc045_a-wa.hs
@@ -0,0 +1,22 @@
+import Control.Monad
+import Control.Applicative ((<$>), (<*>), Alternative, empty)
+import Data.Bits
+import Data.Array.IO
+import Data.Array.MArray
+import qualified Data.List as List
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+($>) = flip ($)
+readLines :: (Read a) => Int -> IO [a]
+readLines n =
+    (concatMap $ fmap read . words) <$> replicateM n getLine
+----
+
+solve [] = []
+solve ("Left" : xs) = '<' : ' ' : solve xs
+solve ("Right": xs) = '>' : ' ' : solve xs
+solve ("AtCoder" : xs) = 'A' : ' ' : solve xs
+
+main =
+    solve . words <$> getLine >>= putStrLn
