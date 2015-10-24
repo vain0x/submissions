d63669e9dac339e2409cdc10e3924d52c4b9fa28
vain0x <vainzerox@gmail.com>
2015-10-24 21:02:55 +0900

    Add https://atcoder.jp/contests/abc030/submissions/535618

---
+++ atcoder/2015-10/24-21-02-55-abc030_a-ac.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <cmath>
+#ifndef ONLINE_JUDGE
+# include <complex>
+# include <random>
+# include <array>
+# include <unordered_map>
+# define mkt make_tuple
+#endif
+#ifdef _LOCAL
+# include "local/local.hpp"
+#else
+# define assert(...) ((void)0)
+# define ifdebug if (false)
+# define echo(...) ((void)0)
+#endif
+using namespace std;
+typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define all(_X) (_X).begin(), (_X).end()
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int cmp = b * c - a * d;
+	cout << (cmp < 0 ? "AOKI" : (cmp > 0 ? "TAKAHASHI" : "DRAW")) << endl;
+
+	return 0;
+}
