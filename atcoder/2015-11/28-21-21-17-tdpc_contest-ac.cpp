cdc5e2cf895a01edfd2aa4a34e8f52759353cd5c
vain0x <vainzerox@gmail.com>
2015-11-28 21:21:17 +0900

    https://beta.atcoder.jp/contests/tdpc/submissions/583293

---
+++ atcoder/2015-11/28-21-21-17-tdpc_contest-ac.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <functional>
+#include <cmath>
+#include <memory>
+#ifndef ONLINE_JUDGE
+# include <complex>
+# include <random>
+# include <array>
+# include <unordered_map>
+# include <unordered_set>
+# define mkt make_tuple
+#endif
+#ifdef _LOCAL
+# include "local/local.hpp"
+#else
+# define assert(...) ((void)0)
+# define ifdebug if (false)
+# define echo(...) ((void)0)
+#endif
+using namespace std;
+typedef long long ll; typedef unsigned long long ull;
+#define repi(_I, _B, _E) for(int _I = (_B); (_I) < (_E); ++ (_I))
+#define rep(_I, _N) for(int _I = 0; (_I) < (_N); ++ (_I))
+#define all(_X) (_X).begin(), (_X).end()
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> ps(n);
+	rep(i, n)
+	{
+		cin >> ps[i];
+	}
+
+	unordered_set<int> scores;
+	scores.emplace(0);
+	rep(i, n)
+	{
+		auto scores_bak = scores;
+		for ( auto&& s : scores_bak ) {
+			scores.emplace(s + ps[i]);
+		}
+	}
+
+	cout << scores.size() << endl;
+	return 0;
+}
