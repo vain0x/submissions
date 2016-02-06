1cf2efcf55a04efb43680f35d2e766b51e365658
vain0x <vainzerox@gmail.com>
2016-02-06 23:42:44 +0900

    https://beta.atcoder.jp/contests/abc033/submissions/631109

---
+++ atcoder/2016-02/06-23-42-44-abc033_b-ce.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+import Data.List as List
+
+main :: IO ()
+main = do
+  n <- read <$> getLine
+  xs <- replicateM n ((\[s, p] -> (s, read p)) . words <$> getLine)
+  let total = sum $ map snd xs
+  let dom = List.find (\(s, p) -> p > total `div` 2) xs
+  let name = case dom of
+        Just (s, p) -> s
+        Nothing -> "atcoder"
+  putStrLn name
