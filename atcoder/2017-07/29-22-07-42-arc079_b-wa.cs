f1b1610955234eedd7ddbda70bab1e2b52dd1822
vain0x <vainzerox@gmail.com>
2017-07-29 22:07:42 +0900

    Add https://atcoder.jp/contests/arc079/submissions/1467385

---
+++ atcoder/2017-07/29-22-07-42-arc079_b-wa.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    #region Standard I/O
+    readonly TextReader input;
+    readonly TextWriter output;
+
+    X[] ReadLine<X>(Func<string, X> func)
+    {
+        return input.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    void WriteLineOne(object obj)
+    {
+        output.WriteLine("{0}", obj);
+    }
+
+    void WriteLineMany(params object[] objects)
+    {
+        output.WriteLine(string.Join(" ", objects.Select(obj => obj.ToString())));
+    }
+    #endregion
+
+    public void Run()
+    {
+        var k = ReadLine(long.Parse)[0];
+        WriteLineOne(2);
+        WriteLineMany(k / 2, (k / 2) + (k % 2) + 1);
+    }
+
+    public Program(TextReader input, TextWriter output)
+    {
+        this.input = input;
+        this.output = output;
+    }
+
+    public static void Main(string[] args)
+    {
+        new Program(Console.In, Console.Out).Run();
+    }
+}
