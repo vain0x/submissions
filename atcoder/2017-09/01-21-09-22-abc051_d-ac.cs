69a5cef0ce82a50182c671f8d511ec2ad94f50f0
vain0x <vainzerox@gmail.com>
2017-09-01 21:09:22 +0900

    Add https://atcoder.jp/contests/abc051/submissions/1555685

---
+++ atcoder/2017-09/01-21-09-22-abc051_d-ac.cs
@@ -0,0 +1,259 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public sealed class Edge
+{
+    public int U, V;
+    public long W;
+
+    public Edge(int u, int v, long w)
+    {
+        U = u;
+        V = v;
+        W = w;
+    }
+}
+
+public sealed partial class Program
+{
+    int n, m;
+    Edge[] edges;
+
+    int Solve()
+    {
+        const long MAX = int.MaxValue;
+
+        var d = n.MakeArray(u => n.MakeArray(v => MAX));
+
+        for (var u = 0; u < n; u++)
+        {
+            d[u][u] = 0;
+        }
+
+        foreach (var e in edges)
+        {
+            d[e.U][e.V] = e.W;
+            d[e.V][e.U] = e.W;
+        }
+
+        for (var t = 0; t < n; t++)
+        {
+            for (var u = 0; u < n; u++)
+            {
+                for (var v = 0; v < n; v++)
+                {
+                    d[u][v] = Math.Min(d[u][v], d[u][t] + d[t][v]);
+                }
+            }
+        }
+
+        return edges.Count(e => d[e.U][e.V] < e.W);
+    }
+
+    void Read()
+    {
+        var a = scanner;
+
+        n = a.N();
+        m = a.N();
+        edges = m.MakeArray(i => new Edge(a.N() - 1, a.N() - 1, a.L()));
+    }
+
+    public void EntryPoint()
+    {
+        Read();
+        WriteLine(Solve());
+    }
+}
+
+#region Custom Library
+public static class TemplateExtension
+{
+    public static X[] MakeArray<X>(this int count, Func<int, X> func)
+    {
+        var xs = new X[count];
+        for (var i = 0; i < count; i++)
+        {
+            xs[i] = func(i);
+        }
+        return xs;
+    }
+
+    public static int[] Range(this int count, int start = 0)
+    {
+        return count.MakeArray(i => i + start);
+    }
+
+    public static string Intercalate<X>(this IEnumerable<X> @this, string separator)
+    {
+        return string.Join(separator, @this);
+    }
+
+    public static void ForEach<X>(this IEnumerable<X> @this, Action<X, int> action)
+    {
+        var list = @this as IReadOnlyList<X>;
+        if (list != null)
+        {
+            var count = list.Count;
+            for (var i = 0; i < count; i++)
+            {
+                action(list[i], i);
+            }
+        }
+        else
+        {
+            var i = 0;
+            foreach (var x in @this)
+            {
+                action(x, i);
+                i++;
+            }
+        }
+    }
+}
+
+public sealed class Scanner
+{
+    readonly TextReader reader;
+    readonly StringBuilder sb = new StringBuilder();
+
+    /// <summary>
+    /// Reads next word separated by spaces.
+    /// </summary>
+    public string Word()
+    {
+        sb.Clear();
+
+        while (true)
+        {
+            var r = reader.Read();
+
+            if (r == '\r')
+            {
+                if (reader.Peek() == '\n') reader.Read();
+                break;
+            }
+            else if (r == -1 || r == ' ' || r == '\n')
+            {
+                break;
+            }
+            else
+            {
+                sb.Append((char)r);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="int"/>.
+    /// </summary>
+    public int N()
+    {
+        return int.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="long"/>.
+    /// </summary>
+    public long L()
+    {
+        return long.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="double"/>.
+    /// </summary>
+    public double F()
+    {
+        return double.Parse(Word());
+    }
+
+    public int[] Ns(int count)
+    {
+        return count.MakeArray(_ => N());
+    }
+
+    public long[] Ls(int count)
+    {
+        return count.MakeArray(_ => L());
+    }
+
+    public double[] Fs(int count)
+    {
+        return count.MakeArray(_ => F());
+    }
+
+    /// <summary>
+    /// Reads next line and splits it by spaces.
+    /// </summary>
+    public X[] Words<X>(Func<string, X> func)
+    {
+        return reader.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    public Scanner(TextReader reader)
+    {
+        this.reader = reader;
+    }
+}
+
+public partial class Program
+{
+    readonly TextReader input;
+    readonly TextWriter output;
+    readonly Scanner scanner;
+
+    void WriteLine(int value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(long value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(double value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(char value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(string value)
+    {
+        output.WriteLine(value);
+    }
+
+    public Program(TextReader input, TextWriter output)
+    {
+        this.input = input;
+        this.output = output;
+        scanner = new Scanner(input);
+    }
+
+    public static void Main(string[] args)
+    {
+#if DEBUG
+        using (var writer = new VainZero.IO.DebugTextWriter(Console.Out))
+#else
+        var writer = Console.Out;
+#endif
+        {
+            new Program(Console.In, writer).EntryPoint();
+        }
+    }
+}
+#endregion
