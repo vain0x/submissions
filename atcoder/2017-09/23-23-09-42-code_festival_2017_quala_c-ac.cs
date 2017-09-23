d70ac361544b78f975115cbde514439f11518ed7
vain0x <vainzerox@gmail.com>
2017-09-23 23:09:42 +0900

    Add https://atcoder.jp/contests/code-festival-2017-quala/submissions/1623128

---
+++ atcoder/2017-09/23-23-09-42-code_festival_2017_quala_c-ac.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TemplateExtension
+{
+    public static X[] MakeArray<X>(this int count, Func<int, X> func)
+    {
+        var xs = new X[count];
+        for (var i = 0; i < count; i++)
+        {
+            xs[i] = func(i);
+        }
+        return xs;
+    }
+
+    public static int[] Range(this int count, int start = 0)
+    {
+        return count.MakeArray(i => i + start);
+    }
+
+    public static string Intercalate<X>(this IEnumerable<X> @this, string separator)
+    {
+        return string.Join(separator, @this);
+    }
+
+    public sealed class ValueIndexPair<T>
+        : Tuple<T, int>
+    {
+        public T Value { get { return Item1; } }
+        public int Index { get { return Item2; } }
+
+        public ValueIndexPair(T value, int index)
+            : base(value, index)
+        {
+        }
+    }
+
+    public static IEnumerable<ValueIndexPair<X>> Indexed<X>(this IEnumerable<X> @this)
+    {
+        var i = 0;
+        foreach (var x in @this)
+        {
+            yield return new ValueIndexPair<X>(x, i);
+            i++;
+        }
+    }
+}
+
+public sealed class Scanner
+{
+    readonly TextReader reader;
+    readonly StringBuilder sb = new StringBuilder();
+
+    /// <summary>
+    /// Reads next word separated by spaces.
+    /// </summary>
+    public string Word()
+    {
+        sb.Clear();
+
+        while (true)
+        {
+            var r = reader.Read();
+
+            if (r == '\r')
+            {
+                if (reader.Peek() == '\n') reader.Read();
+                break;
+            }
+            else if (r == -1 || r == ' ' || r == '\n')
+            {
+                break;
+            }
+            else
+            {
+                sb.Append((char)r);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="int"/>.
+    /// </summary>
+    public int N()
+    {
+        return int.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="long"/>.
+    /// </summary>
+    public long L()
+    {
+        return long.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="double"/>.
+    /// </summary>
+    public double F()
+    {
+        return double.Parse(Word());
+    }
+
+    public int[] Ns(int count)
+    {
+        return count.MakeArray(_ => N());
+    }
+
+    public long[] Ls(int count)
+    {
+        return count.MakeArray(_ => L());
+    }
+
+    public double[] Fs(int count)
+    {
+        return count.MakeArray(_ => F());
+    }
+
+    /// <summary>
+    /// Reads next line and splits it by spaces.
+    /// </summary>
+    public X[] Words<X>(Func<string, X> func)
+    {
+        return reader.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    public Scanner(TextReader reader)
+    {
+        this.reader = reader;
+    }
+}
+
+public partial class Program
+{
+    readonly TextReader input;
+    readonly TextWriter output;
+    readonly Scanner scanner;
+
+    void WriteLine(int value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(long value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(double value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(char value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(string value)
+    {
+        output.WriteLine(value);
+    }
+
+    public Program(TextReader input, TextWriter output)
+    {
+        this.input = input;
+        this.output = output;
+        scanner = new Scanner(input);
+    }
+
+    public static void Main(string[] args)
+    {
+#if DEBUG
+        using (var writer = new VainZero.IO.DebugTextWriter(Console.Out))
+#else
+        var writer = Console.Out;
+#endif
+        {
+            new Program(Console.In, writer).EntryPoint();
+        }
+    }
+}
+
+public sealed partial class Program
+{
+    int h, w;
+    int[] ks = new int[26];
+
+    bool Solve()
+    {
+        var he = h % 2 == 0;
+        var we = w % 2 == 0;
+        var ls = ks.Where(k => k % 4 != 0).ToArray();
+
+        if (he && we)
+        {
+            // h, w: 偶数
+            return !ls.Any();
+
+        }
+        else if (he || we)
+        {
+            // h, w: 片方が偶数、他方が奇数
+            var c = (he ? h : w) / 2;
+            return ls.All(k => k % 2 == 0) && ls.Length <= c;
+        }
+        else
+        {
+            // h, w: 両方奇数
+            var b = (h - 1 + w - 1) / 2;
+            return
+                ls.Count(k => k % 2 != 0) == 1
+                && ls.Count(k => k % 4 == 2 || k % 4 == 3) <= b;
+        }
+    }
+
+    void Read()
+    {
+        var a = scanner;
+
+        h = a.N();
+        w = a.N();
+
+        for (var i = 0; i < h; i++)
+        {
+            var line = input.ReadLine();
+            foreach (var c in line)
+            {
+                ks[c - 'a']++;
+            }
+        }
+    }
+
+    public void EntryPoint()
+    {
+        Read();
+        WriteLine(Solve() ? "Yes" : "No");
+    }
+}
