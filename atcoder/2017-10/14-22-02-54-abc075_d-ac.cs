2ff48e8347bcb9d401e6a01190874701c5696121
vain0x <vainzerox@gmail.com>
2017-10-14 22:02:54 +0900

    https://beta.atcoder.jp/contests/abc075/submissions/1684299

---
+++ atcoder/2017-10/14-22-02-54-abc075_d-ac.cs
@@ -0,0 +1,427 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TemplateExtension
+{
+    public static X[] MakeArray<X>(this int count, Func<int, X> func)
+    {
+        var xs = new X[count];
+        for (var i = 0; i < count; i++)
+        {
+            xs[i] = func(i);
+        }
+        return xs;
+    }
+
+    public static int[] Range(this int count, int start = 0)
+    {
+        return count.MakeArray(i => i + start);
+    }
+
+    public static string Intercalate<X>(this IEnumerable<X> @this, string separator)
+    {
+        return string.Join(separator, @this);
+    }
+
+    public sealed class ValueIndexPair<T>
+        : Tuple<T, int>
+    {
+        public T Value { get { return Item1; } }
+        public int Index { get { return Item2; } }
+
+        public ValueIndexPair(T value, int index)
+            : base(value, index)
+        {
+        }
+    }
+
+    public static IEnumerable<ValueIndexPair<X>> Indexed<X>(this IEnumerable<X> @this)
+    {
+        var i = 0;
+        foreach (var x in @this)
+        {
+            yield return new ValueIndexPair<X>(x, i);
+            i++;
+        }
+    }
+}
+
+public sealed class Scanner
+{
+    readonly TextReader reader;
+    readonly StringBuilder sb = new StringBuilder();
+
+    /// <summary>
+    /// Reads next word separated by spaces.
+    /// </summary>
+    public string Word()
+    {
+        sb.Clear();
+
+        while (true)
+        {
+            var r = reader.Read();
+
+            if (r == '\r')
+            {
+                if (reader.Peek() == '\n') reader.Read();
+                break;
+            }
+            else if (r == -1 || r == ' ' || r == '\n')
+            {
+                break;
+            }
+            else
+            {
+                sb.Append((char)r);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="int"/>.
+    /// </summary>
+    public int N()
+    {
+        return int.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="long"/>.
+    /// </summary>
+    public long L()
+    {
+        return long.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="double"/>.
+    /// </summary>
+    public double F()
+    {
+        return double.Parse(Word());
+    }
+
+    public int[] Ns(int count)
+    {
+        return count.MakeArray(_ => N());
+    }
+
+    public long[] Ls(int count)
+    {
+        return count.MakeArray(_ => L());
+    }
+
+    public double[] Fs(int count)
+    {
+        return count.MakeArray(_ => F());
+    }
+
+    /// <summary>
+    /// Reads next line and splits it by spaces.
+    /// </summary>
+    public X[] Words<X>(Func<string, X> func)
+    {
+        return reader.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    public Scanner(TextReader reader)
+    {
+        this.reader = reader;
+    }
+}
+
+public partial class Program
+{
+    readonly TextReader input;
+    readonly TextWriter output;
+    readonly Scanner scanner;
+
+    void WriteLine(int value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(long value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(double value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(char value)
+    {
+        output.WriteLine(value);
+    }
+
+    void WriteLine(string value)
+    {
+        output.WriteLine(value);
+    }
+
+    public Program(TextReader input, TextWriter output)
+    {
+        this.input = input;
+        this.output = output;
+        scanner = new Scanner(input);
+    }
+
+    public static void Main(string[] args)
+    {
+#if DEBUG
+        using (var writer = new VainZero.IO.DebugTextWriter(Console.Out))
+#else
+        var writer = Console.Out;
+#endif
+        {
+            new Program(Console.In, writer).EntryPoint();
+        }
+    }
+}
+
+
+
+
+/// <summary>
+/// Provides binary search implementations.
+/// </summary>
+public static class BinarySearch
+{
+    /// <summary>
+    /// Performs Meguru-style binary search.
+    /// <seealso cref="https://twitter.com/meguru_comp/status/697008509376835584"/>
+    /// </summary>
+    public static int Meguru(int ok, int ng, Func<int, bool> isOk)
+    {
+        while (Math.Abs(ok - ng) > 1)
+        {
+            var mid = (ok + ng) / 2;
+            if (isOk(mid))
+            {
+                ok = mid;
+            }
+            else
+            {
+                ng = mid;
+            }
+        }
+        return ok;
+    }
+
+    /// <summary>
+    /// Performs Meguru-style binary search.
+    /// </summary>
+    public static long Meguru(long ok, long ng, Func<long, bool> isOk)
+    {
+        while (Math.Abs(ok - ng) > 1)
+        {
+            var mid = (ok + ng) / 2;
+            if (isOk(mid))
+            {
+                ok = mid;
+            }
+            else
+            {
+                ng = mid;
+            }
+        }
+        return ok;
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the lower bound index of the specified value in the range.
+    /// </summary>
+    public static int LowerBound<X>(this IReadOnlyList<X> @this, X value, int lb, int ub, IComparer<X> comparer)
+    {
+        if (lb > ub) throw new ArgumentOutOfRangeException(nameof(ub));
+
+        while (lb != ub)
+        {
+            var m = lb + (ub - lb) / 2;
+            if (comparer.Compare(@this[m], value) < 0)
+            {
+                lb = m + 1;
+            }
+            else
+            {
+                ub = m;
+            }
+        }
+
+        return lb;
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the lower bound index of the specified value.
+    /// </summary>
+    public static int LowerBound<X>(this IReadOnlyList<X> @this, X value, IComparer<X> comparer)
+    {
+        return LowerBound(@this, value, 0, @this.Count, comparer);
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the lower bound index of the specified value in the range.
+    /// </summary>
+    public static int LowerBound<X>(this IReadOnlyList<X> @this, X value, int lb, int ub)
+    {
+        return LowerBound(@this, value, lb, ub, Comparer<X>.Default);
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the lower bound index of the specified value.
+    /// </summary>
+    public static int LowerBound<X>(this IReadOnlyList<X> @this, X value)
+    {
+        return LowerBound(@this, value, 0, @this.Count, Comparer<X>.Default);
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the upper bound index of the specified value in the range.
+    /// </summary>
+    public static int UpperBound<X>(this IReadOnlyList<X> @this, X value, int lb, int ub, IComparer<X> comparer)
+    {
+        if (lb > ub) throw new ArgumentOutOfRangeException(nameof(ub));
+
+        while (lb != ub)
+        {
+            var m = lb + (ub - lb) / 2;
+            if (comparer.Compare(@this[m], value) <= 0)
+            {
+                lb = m + 1;
+            }
+            else
+            {
+                ub = m;
+            }
+        }
+
+        return lb;
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the upper bound index of the specified value.
+    /// </summary>
+    public static int UpperBound<X>(this IReadOnlyList<X> @this, X value, IComparer<X> comparer)
+    {
+        return UpperBound(@this, value, 0, @this.Count, comparer);
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the upper bound index of the specified value in the range.
+    /// </summary>
+    public static int UpperBound<X>(this IReadOnlyList<X> @this, X value, int lb, int ub)
+    {
+        return UpperBound(@this, value, lb, ub, Comparer<X>.Default);
+    }
+
+    /// <summary>
+    /// Performs binary search
+    /// to find the upper bound index of the specified value.
+    /// </summary>
+    public static int UpperBound<X>(this IReadOnlyList<X> @this, X value)
+    {
+        return UpperBound(@this, value, 0, @this.Count, Comparer<X>.Default);
+    }
+}
+
+public sealed partial class Program
+{
+    int n, k;
+    Tuple<long, long>[] ps;
+    long[] xs;
+    long[] ys;
+
+    ILookup<long, long> ysFromX;
+
+    int CountPoints(int yl, int yr, int xi)
+    {
+        return ysFromX[xs[xi]].Count(y => ys[yl] <= y && y <= ys[yr]);
+    }
+
+    long[] Gen(Func<Tuple<long, long>, long> selector)
+    {
+        return
+            (
+                from p in ps
+                let x = selector(p)
+                from x2 in new[] { x - 1, x, x + 1 }
+                select x2
+            )
+            .Distinct()
+            .OrderBy(x => x)
+            .ToArray();
+    }
+
+    long Solve()
+    {
+        xs = Gen(p => p.Item1);
+        ys = Gen(p => p.Item2);
+
+        ysFromX = ps.ToLookup(p => p.Item1, p => p.Item2);
+
+        var min = long.MaxValue;
+
+        for (var yl = 0; yl < ys.Length; yl++)
+        {
+            for (var yr = yl + 1; yr < ys.Length; yr++)
+            {
+                var xl = 0;
+                var xr = 0;
+                var count = CountPoints(yl, yr, 0);
+
+                while (true)
+                {
+                    while (xr + 1 < xs.Length && (xl == xr || count < k))
+                    {
+                        xr++;
+                        count += CountPoints(yl, yr, xr);
+                    }
+
+                    if (xl == xr || count < k) break;
+
+                    var size = (xs[xr] - xs[xl]) * (ys[yr] - ys[yl]);
+                    min = Math.Min(min, size);
+
+                    xl++;
+                    count -= CountPoints(yl, yr, xl - 1);
+                }
+            }
+        }
+
+        return min;
+    }
+
+    void Read()
+    {
+        var a = scanner;
+
+        n = a.N();
+        k = a.N();
+        ps = n.MakeArray(i => Tuple.Create(a.L(), a.L()));
+    }
+
+    public void EntryPoint()
+    {
+        Read();
+        WriteLine(Solve());
+    }
+}
