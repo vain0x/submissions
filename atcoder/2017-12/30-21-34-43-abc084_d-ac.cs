2dcf5138e0a412354fc04d031f0dd694efe79048
vain0x <vainzerox@gmail.com>
2017-12-30 21:34:43 +0900

    https://beta.atcoder.jp/contests/abc084/submissions/1924718

---
+++ atcoder/2017-12/30-21-34-43-abc084_d-ac.cs
@@ -0,0 +1,235 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class TemplateExtension
+{
+    public static X[] MakeArray<X>(this int count, Func<int, X> func)
+    {
+        var xs = new X[count];
+        for (var i = 0; i < count; i++)
+        {
+            xs[i] = func(i);
+        }
+        return xs;
+    }
+
+    public static int[] Range(this int count, int start = 0)
+    {
+        return count.MakeArray(i => i + start);
+    }
+
+    public static string Intercalate<X>(this IEnumerable<X> @this, string separator)
+    {
+        return string.Join(separator, @this);
+    }
+
+    public sealed class ValueIndexPair<T>
+        : Tuple<T, int>
+    {
+        public T Value { get { return Item1; } }
+        public int Index { get { return Item2; } }
+
+        public ValueIndexPair(T value, int index)
+            : base(value, index)
+        {
+        }
+    }
+
+    public static IEnumerable<ValueIndexPair<X>> Indexed<X>(this IEnumerable<X> @this)
+    {
+        var i = 0;
+        foreach (var x in @this)
+        {
+            yield return new ValueIndexPair<X>(x, i);
+            i++;
+        }
+    }
+}
+
+public sealed class Scanner
+{
+    private readonly TextReader _reader;
+    private readonly StringBuilder _sb = new StringBuilder();
+
+    /// <summary>
+    /// Reads next word separated by spaces.
+    /// </summary>
+    public string Word()
+    {
+        _sb.Clear();
+
+        while (true)
+        {
+            var r = _reader.Read();
+
+            if (r == ' ' || r == '\r' || r == '\n')
+            {
+                if (r == '\r' && _reader.Peek() == '\n')
+                {
+                    _reader.Read();
+                }
+
+                // Ignore leading spaces.
+                if (_sb.Length == 0) continue;
+
+                break;
+            }
+            else if (r == -1)
+            {
+                break;
+            }
+            else
+            {
+                _sb.Append((char)r);
+            }
+        }
+
+        return _sb.ToString();
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="int"/>.
+    /// </summary>
+    public int N()
+    {
+        return int.Parse(Word().Trim());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="long"/>.
+    /// </summary>
+    public long L()
+    {
+        return long.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next word as <see cref="double"/>.
+    /// </summary>
+    public double F()
+    {
+        return double.Parse(Word());
+    }
+
+    /// <summary>
+    /// Reads next line and splits it by spaces.
+    /// </summary>
+    public X[] Words<X>(Func<string, X> func)
+    {
+        return _reader.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    public Scanner(TextReader reader)
+    {
+        _reader = reader;
+    }
+}
+
+public partial class Program
+{
+    private readonly TextReader _input;
+    private readonly TextWriter _output;
+    private readonly Scanner _scanner;
+
+    private void WriteLine(int value)
+    {
+        _output.WriteLine(value);
+    }
+
+    private void WriteLine(long value)
+    {
+        _output.WriteLine(value);
+    }
+
+    private void WriteLine(double value)
+    {
+        _output.WriteLine(value);
+    }
+
+    private void WriteLine(char value)
+    {
+        _output.WriteLine(value);
+    }
+
+    private void WriteLine(string value)
+    {
+        _output.WriteLine(value);
+    }
+
+    public Program(TextReader input, TextWriter output)
+    {
+        _input = input;
+        _output = output;
+        _scanner = new Scanner(input);
+    }
+
+    public static void Main(string[] args)
+    {
+        new Program(Console.In, Console.Out).EntryPoint();
+    }
+}
+
+public sealed partial class Program
+{
+    int Q;
+    int[] L;
+    int[] R;
+
+    private IEnumerable<int> Solve()
+    {
+        var M = R.Max() + 1;
+
+        var isPrime = M.MakeArray(i => true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (var n = 2; n < M; n++)
+        {
+            if (!isPrime[n]) continue;
+
+            for (var j = 2; j * n < M; j++)
+            {
+                isPrime[j * n] = false;
+            }
+        }
+
+        var good = M.MakeArray(n => n % 2 == 1 && isPrime[n] && isPrime[(n + 1) / 2]);
+
+        var s = new int[M];
+        for (var i = 1; i < M; i++)
+        {
+            s[i] = s[i - 1] + (good[i - 1] ? 1 : 0);
+        }
+
+        for (var qi = 0; qi < Q; qi++)
+        {
+            yield return s[R[qi]] - s[L[qi]];
+        }
+    }
+
+    public void EntryPoint()
+    {
+        var I = _scanner;
+        Q = I.N();
+
+        L = new int[Q];
+        R = new int[Q];
+        for (var i = 0; i < Q; i++)
+        {
+            L[i] = I.N();
+            R[i] = I.N() + 1;
+        }
+
+        foreach (var x in Solve())
+        {
+            WriteLine(x);
+        }
+    }
+}
