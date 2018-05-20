9f3e1213527515fb1f2b24fec3d7255ff8577738
vain0x <vainzerox@gmail.com>
2018-05-20 21:32:34 +0900

    Add https://atcoder.jp/contests/agc024/submissions/2535771

---
+++ atcoder/2018-05/20-21-32-34-agc024_a-ac.rs
@@ -0,0 +1,54 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use procon::*;
+use std::cmp::{max, min};
+use std::collections::*;
+use std::ops::*;
+use std::*;
+
+pub fn main() {
+    let words = read_words::<i64>();
+    let (A, B, _, K) = (words[0], words[1], words[2], words[3]);
+    let parity = K % 2;
+    println!("{}", if parity == 0 { A - B } else { B - A });
+    return;
+}
+
+pub mod procon {
+    use std;
+    use std::collections::*;
+    use std::io;
+    use std::mem;
+    use std::str::FromStr;
+
+    pub fn read_line() -> String {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        buf.trim_right().to_owned()
+    }
+
+    pub fn read_words<T>() -> Vec<T>
+    where
+        T: std::str::FromStr,
+        T::Err: std::fmt::Debug,
+    {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        buf.split_whitespace()
+            .map(|word| T::from_str(word).unwrap())
+            .collect()
+    }
+
+    pub fn read_vec(len: usize) -> Vec<String> {
+        let mut vec = Vec::new();
+        while vec.len() < len {
+            let line = read_line();
+            for word in line.split_whitespace() {
+                vec.push(word.to_owned());
+            }
+        }
+        assert!(vec.len() == len);
+        vec
+    }
+}
