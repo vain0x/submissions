83d62ccce37d1e679d7647440ee3d0d76bce92f3
vain0x <vainzerox@gmail.com>
2018-06-07 19:16:07 +0900

    Add https://atcoder.jp/contests/code-festival-2017-qualc/submissions/2630491

---
+++ atcoder/2018-06/07-19-16-07-code_festival_2017_qualc_c-ac.rs
@@ -0,0 +1,147 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::io::*;
+use std::ops::*;
+use std::*;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+#[allow(unused)]
+fn rw<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|word| T::from_str(word).unwrap())
+        .collect()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = &[
+                $((
+                    &stringify!($arg).to_string() as &fmt::Debug,
+                    &($arg) as &fmt::Debug,
+                )),*
+            ];
+            eprintln!("{:?}", DebugMap(entries));
+        }
+    };
+}
+
+#[allow(unused)]
+struct DebugMap<'a>(&'a [(&'a fmt::Debug, &'a fmt::Debug)]);
+
+impl<'a> std::fmt::Debug for DebugMap<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let mut m = fmt.debug_map();
+        for &(key, value) in self.0.iter() {
+            m.entry(key, value);
+        }
+        m.finish()
+    }
+}
+
+// -----------------------------------------------
+// Polyfill
+// -----------------------------------------------
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+#[allow(unused)]
+macro_rules! eprintln {
+    ($($arg:expr),*) => { _eprintln(format_args!($($arg),*)) }
+}
+
+fn _eprintln(args: fmt::Arguments) {
+    let err = std::io::stderr();
+    let mut err = err.lock();
+    err.write_fmt(args).unwrap();
+    err.write(b"\n").unwrap();
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let S = rl().chars().collect::<Vec<char>>();
+    let N = S.len();
+    let k = if N == 1 {
+        0
+    } else {
+        let mut l = 0;
+        let mut r = N - 1;
+        let mut k = 0;
+
+        while l < r {
+            if S[l] == S[r] {
+                l += 1;
+                r -= 1;
+            } else if S[l] == 'x' {
+                l += 1;
+                k += 1;
+            } else if S[r] == 'x' {
+                r -= 1;
+                k += 1;
+            } else {
+                k = -1;
+                break;
+            }
+        }
+
+        k
+    };
+
+    println!("{}", k);
+    return;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        assert_eq!(7, 1 + 2 * 3);
+    }
+}
