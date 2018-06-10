198f22b4bb8f7a948eb9cf1f8ed8704ed47d81aa
vain0x <vainzerox@gmail.com>
2018-06-10 21:32:35 +0900

    https://beta.atcoder.jp/contests/abc099/submissions/2648429

---
+++ atcoder/2018-06/10-21-32-35-abc099_c-ac.rs
@@ -0,0 +1,146 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::io::*;
+use std::ops::*;
+use std::*;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+#[allow(unused)]
+fn rw<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|word| T::from_str(word).unwrap())
+        .collect()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = &[
+                $((
+                    &stringify!($arg).to_string() as &fmt::Debug,
+                    &($arg) as &fmt::Debug,
+                )),*
+            ];
+            eprintln!("{:?}", DebugMap(entries));
+        }
+    };
+}
+
+#[allow(unused)]
+struct DebugMap<'a>(&'a [(&'a fmt::Debug, &'a fmt::Debug)]);
+
+impl<'a> std::fmt::Debug for DebugMap<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let mut m = fmt.debug_map();
+        for &(key, value) in self.0.iter() {
+            m.entry(key, value);
+        }
+        m.finish()
+    }
+}
+
+// -----------------------------------------------
+// Polyfill
+// -----------------------------------------------
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+#[allow(unused)]
+macro_rules! eprintln {
+    ($($arg:expr),*) => { _eprintln(format_args!($($arg),*)) }
+}
+
+fn _eprintln(args: fmt::Arguments) {
+    let err = std::io::stderr();
+    let mut err = err.lock();
+    err.write_fmt(args).unwrap();
+    err.write(b"\n").unwrap();
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let n = rl().parse::<usize>().unwrap();
+    let mut u: Vec<usize> = Vec::new();
+    let mut s = 6;
+    let mut t = 9;
+    while s <= n {
+        u.push(s);
+        s *= 6;
+    }
+    while t <= n {
+        u.push(t);
+        t *= 9;
+    }
+    u.push(1);
+    u.sort();
+
+    // dp[n] = k <=> ちょうど n 円引き出すために必要な最小の手数が k
+    let mut dp = vec![n; n + 1];
+    dp[0] = 0;
+
+    for i in 0..n + 1 {
+        for u in u.iter() {
+            if i + u <= n {
+                dp[i + u] = min(dp[i + u], dp[i] + 1);
+            }
+        }
+    }
+
+    println!("{}", dp[n]);
+    return;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        assert_eq!(7, 1 + 2 * 3);
+    }
+}
