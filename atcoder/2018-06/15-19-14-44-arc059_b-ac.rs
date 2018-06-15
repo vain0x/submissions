3c272153ebeb55a0d5dd7c19dec7ffe5a890c4d3
vain0x <vainzerox@gmail.com>
2018-06-15 19:14:44 +0900

    https://beta.atcoder.jp/contests/arc059/submissions/2670165

---
+++ atcoder/2018-06/15-19-14-44-arc059_b-ac.rs
@@ -0,0 +1,83 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter};
+use std::io::*;
+use std::ops::*;
+use std::*;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = [$(&stringify!([$arg]:), &$arg as &Debug),*];
+            stderr().write_fmt(format_args!("{:#?}\n", entries)).unwrap();
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let S = rl().chars().vec();
+
+    let mut r = None;
+    for i in 0..S.len() {
+        if i + 1 < S.len() && S[i] == S[i + 1] {
+            r = Some((i + 1, i + 2));
+            break;
+        }
+
+        if i + 2 < S.len() && S[i] == S[i + 2] {
+            r = Some((i + 1, i + 3));
+            break;
+        }
+    }
+
+    match r {
+        Some((l, r)) => println!("{} {}", l, r),
+        None => println!("-1 -1"),
+    }
+    return;
+}
