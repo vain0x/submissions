083741f8967463d2686f3c42fc82893c5ba87524
vain0x <vainzerox@gmail.com>
2018-06-17 22:02:20 +0900

    https://beta.atcoder.jp/contests/agc003/submissions/2695045

---
+++ atcoder/2018-06/17-22-02-20-agc003_b-ac.rs
@@ -0,0 +1,71 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter};
+use std::io::*;
+use std::ops::*;
+use std::*;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = [$(&stringify!([$arg]:), &$arg as &Debug),*];
+            stderr().write_fmt(format_args!("{:#?}\n", entries)).unwrap();
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let N = read!(usize);
+    let A = read!(i64; N);
+
+    let k = A.split(|&x| x == 0)
+        .map(|a| a.iter().sum::<i64>() / 2)
+        .sum::<i64>();
+    println!("{}", k);
+    return;
+}
