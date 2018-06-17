e7161f626e89b7bcaa4f3af3867059f5978e3cec
vain0x <vainzerox@gmail.com>
2018-06-17 21:44:20 +0900

    Add https://atcoder.jp/contests/agc003/submissions/2694966

---
+++ atcoder/2018-06/17-21-44-20-agc003_b-ac.rs
@@ -0,0 +1,90 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter};
+use std::io::*;
+use std::ops::*;
+use std::*;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = [$(&stringify!([$arg]:), &$arg as &Debug),*];
+            stderr().write_fmt(format_args!("{:#?}\n", entries)).unwrap();
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let N = read!(usize);
+    let mut A = read!(i64; N);
+
+    let mut l = 0;
+    let mut k = 0;
+    while l < N {
+        if A[l] % 2 == 0 {
+            k += A[l] / 2;
+            l += 1;
+        } else {
+            let mut r = l + 1;
+            while r < N && A[r] != 0 && A[r] % 2 == 0 {
+                r += 1;
+            }
+
+            if r == N || A[r] == 0 {
+                A[l] -= 1;
+            } else {
+                A[l] -= 1;
+                A[r] -= 1;
+                k += 1;
+            }
+        }
+    }
+
+    println!("{}", k);
+    return;
+}
