b1a47ccba91e30c614a36f7e7a83db510a970e72
vain0x <vainzerox@gmail.com>
2018-07-21 21:44:08 +0900

    https://beta.atcoder.jp/contests/abc103/submissions/2883017

---
+++ atcoder/2018-07/21-21-44-08-abc103_b-ac.rs
@@ -0,0 +1,88 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter};
+use std::io::*;
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = [$(&stringify!([$arg]:), &$arg as &Debug),*];
+            stderr().write_fmt(format_args!("{:#?}\n", entries)).unwrap();
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let S = rl().chars().vec();
+    let T = rl().chars().vec();
+
+    for i in 0..S.len() {
+        let mut ok = true;
+
+        for j in 0..T.len() {
+            if S[(i + j) % S.len()] == T[j] {
+                continue;
+            }
+
+            ok = false;
+            break;
+        }
+
+        if ok {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
+
+    return;
+}
