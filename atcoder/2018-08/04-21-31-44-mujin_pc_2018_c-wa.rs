b60c8b651a0a3b69ada8c20b2c34a02d958c2292
vain0x <vainzerox@gmail.com>
2018-08-04 21:31:44 +0900

    https://beta.atcoder.jp/contests/mujin-pc-2018/submissions/2943881

---
+++ atcoder/2018-08/04-21-31-44-mujin_pc_2018_c-wa.rs
@@ -0,0 +1,146 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter};
+use std::io::*;
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        {
+            let entries = [$(&stringify!([$arg]:), &$arg as &Debug),*];
+            stderr().write_fmt(format_args!("{:#?}\n", entries)).unwrap();
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn main() {
+    let D: Vec<(isize, isize)> = vec![(1, 0), (0, 1), (-1, 0), (0, -1)];
+
+    let (H, W) = read!(usize, usize);
+    let S = read![String; H];
+    let board_core = S
+        .into_iter()
+        .map(|s| s.chars().map(|c| c != '#').vec())
+        .vec();
+
+    let mut board = vec![vec![false; W + 2]; H + 2];
+    for y in 0..H {
+        for x in 0..W {
+            board[y + 1][x + 1] = board_core[y][x];
+        }
+    }
+
+    let mut space_up = vec![vec![0; W + 3]; H + 3];
+    let mut space_down = vec![vec![0; W + 3]; H + 3];
+    let mut space_right = vec![vec![0; W + 3]; H + 3];
+    let mut space_left = vec![vec![0; W + 3]; H + 3];
+
+    for x in 1..W + 1 {
+        for y in 1..H + 1 {
+            space_down[y][x] += if board[y][x] {
+                space_down[y - 1][x] + 1
+            } else {
+                0
+            };
+        }
+
+        for y in (1..H + 1).rev() {
+            space_up[y][x] += if board[y][x] {
+                space_up[y + 1][x] + 1
+            } else {
+                0
+            };
+        }
+    }
+
+    for y in 1..H + 1 {
+        for x in 1..W + 1 {
+            space_right[y][x] += if board[y][x] {
+                space_right[y][x - 1] + 1
+            } else {
+                0
+            };
+        }
+
+        for x in (1..W + 1).rev() {
+            space_left[y][x] += if board[y][x] {
+                space_left[y][x + 1] + 1
+            } else {
+                0
+            };
+        }
+    }
+
+    let space_count = |d, y: usize, x: usize| match d {
+        0 | 4 => space_down[y][x],
+        1 => space_right[y][x],
+        2 => space_up[y][x],
+        3 => space_left[y][x],
+        _ => unreachable!(),
+    } - 1;
+
+    let mut sum = 0;
+    for y in 1..H + 1 {
+        for x in 1..W + 1 {
+            if !board[y][x] {
+                continue;
+            }
+
+            for d1 in 0..4 {
+                let d2 = (d1 + 1) % 4;
+                sum += space_count(d1, y, x) * space_count(d2, y, x);
+            }
+        }
+    }
+
+    println!("{}", sum);
+    return;
+}
