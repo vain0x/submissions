5941bd187e0340b7abebf4ac8724413493b28429
vain0x <vainzerox@gmail.com>
2018-08-18 21:24:37 +0900

    Add https://atcoder.jp/contests/abc106/submissions/3033089

---
+++ atcoder/2018-08/18-21-24-37-abc106_d-wa.rs
@@ -0,0 +1,87 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        $(writeln!(stderr(), "{} = {:?}", stringify!($arg), $arg).unwrap());*
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let (N, M, Q) = read!(usize, usize, usize);
+
+    let mut part = vec![0; N + 1];
+    let mut comp = vec![0; N + 1];
+
+    for _ in 0..M {
+        let (l, r) = read!(usize, usize);
+
+        part[l] += 1;
+        comp[r] += 1;
+    }
+
+    for i in 0..N {
+        part[1 + i] += part[i];
+        comp[1 + i] += comp[i];
+    }
+
+    // debug!(part, comp);
+
+    for _ in 0..Q {
+        let (p, q) = read!(usize, usize);
+        let p = p - 1;
+
+        println!("{}", comp[q] - part[p])
+    }
+}
