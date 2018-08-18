cce884bdf865de831cb1a61cf8002cd18e3b2d74
vain0x <vainzerox@gmail.com>
2018-08-18 21:09:09 +0900

    Add https://atcoder.jp/contests/abc106/submissions/3030466

---
+++ atcoder/2018-08/18-21-09-09-abc106_b-ac.rs
@@ -0,0 +1,89 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        $(writeln!(stderr(), "{} = {:?}", stringify!($arg), $arg).unwrap());*
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn f(n: i32) -> Vec<i32> {
+    let mut v = vec![];
+
+    for k in 1..n + 1 {
+        if n % k == 0 {
+            v.push(k);
+        }
+    }
+
+    v
+}
+
+fn main() {
+    let N = read!(i32);
+
+    let mut count = 0;
+
+    for n in 1..N + 1 {
+        if n % 2 == 0 {
+            continue;
+        }
+
+        if f(n).len() == 8 {
+            count += 1;
+        }
+    }
+
+    println!("{}", count)
+}
