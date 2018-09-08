5a58aa1eb3fb842f81a85216bbbb02c601b2108a
vain0x <vainzerox@gmail.com>
2018-09-08 21:08:44 +0900

    Add https://atcoder.jp/contests/abc109/submissions/3153621

---
+++ atcoder/2018-09/08-21-08-44-abc109_b-ac.rs
@@ -0,0 +1,86 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($arg:expr),*) => {
+        #[cfg(debug_assertions)]
+        $(writeln!(stderr(), "{} = {:?}", stringify!($arg), $arg).unwrap());*
+    };
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let N = read!(usize);
+
+    let mut ws = HashSet::new();
+    let w = rl();
+    let mut last = *w.as_bytes().last().unwrap();
+    ws.insert(w);
+    let mut ok = true;
+
+    for _ in 1..N {
+        let w = rl();
+        if w.as_bytes()[0] != last {
+            ok = false;
+            break;
+        }
+        if ws.contains(&w) {
+            ok = false;
+            break;
+        }
+
+        last = w.as_bytes().last().cloned().unwrap();
+        ws.insert(w);
+    }
+
+    println!("{}", if ok { "Yes" } else { "No" })
+}
