86116e18417bce32e9a36452de31f6f6d0e218b1
vain0x <vainzerox@gmail.com>
2018-10-06 21:22:01 +0900

    https://beta.atcoder.jp/contests/abc112/submissions/3346451

---
+++ atcoder/2018-10/06-21-22-01-abc112_d-wa.rs
@@ -0,0 +1,74 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[cfg(debug_assertions)]
+include!{"./procon/debug.rs"}
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ($($arg:expr),*) => {};
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn gcd(x: usize, y: usize) -> usize {
+    if y == 0 {
+        x
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn main() {
+    let (N, M) = read!(usize, usize);
+    let x = M / N;
+    let g = if M % N == 0 { x } else { gcd(x, x + M % N) };
+    println!("{}", g)
+}
