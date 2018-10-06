40b6663301ee452f5a4dcf479e29465a610d3b57
vain0x <vainzerox@gmail.com>
2018-10-06 22:06:54 +0900

    Add https://atcoder.jp/contests/abc112/submissions/3351886

---
+++ atcoder/2018-10/06-22-06-54-abc112_b-ac.rs
@@ -0,0 +1,79 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[cfg(debug_assertions)]
+include!{"./procon/debug.rs"}
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ($($arg:expr),*) => {};
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let (N, T) = read!(usize, i64);
+    let R = read!(i64, i64; N);
+
+    let mut mi = std::i64::MAX;
+    let mut tle = true;
+    for (c, t) in R {
+        if t <= T {
+            mi = min(mi, c);
+            tle = false;
+        }
+    }
+
+    if tle {
+        println!("TLE");
+    } else {
+        println!("{}", mi)
+    }
+}
