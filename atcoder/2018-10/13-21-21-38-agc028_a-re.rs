e50e2f1e45d01ca0d50f2e24283523abba42aca0
vain0x <vainzerox@gmail.com>
2018-10-13 21:21:38 +0900

    Add https://atcoder.jp/contests/agc028/submissions/3393902

---
+++ atcoder/2018-10/13-21-21-38-agc028_a-re.rs
@@ -0,0 +1,89 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[cfg(debug_assertions)]
+include!{"./procon/debug.rs"}
+
+#[cfg(not(debug_assertions))]
+macro_rules! debug {
+    ($($arg:expr),*) => {};
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($t:ty) =>
+        (rl().parse::<$t>().unwrap());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+trait IteratorExt: Iterator + Sized {
+    fn vec(self) -> Vec<Self::Item> {
+        self.collect()
+    }
+}
+
+impl<T: Iterator> IteratorExt for T {}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+pub fn gcd(x: usize, y: usize) -> usize {
+    if y == 0 {
+        x
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn main() {
+    let (N, M) = read!(usize, usize);
+    let S = rl().chars().vec();
+    let T = rl().chars().vec();
+
+    let g = gcd(N, M);
+    let l = N / g * M;
+    let g = l;
+
+    let mut u = vec!['.'; g];
+    for i in 0..N {
+        u[i * g / N] = S[i];
+    }
+    // debug!(g, l, u);
+    let mut ok = true;
+    for i in 0..M {
+        let j = i * g / M;
+        ok = ok && (u[j] == '.' || u[j] == T[i]);
+    }
+
+    println!("{}", if ok { g as isize } else { -1 })
+}
