83db5c49c95dde9b4269db30ce498c8b37fb0b41
vain0x <vainzerox@gmail.com>
2018-11-04 21:17:32 +0900

    https://beta.atcoder.jp/contests/abc113/submissions/3535371

---
+++ atcoder/2018-11/04-21-17-32-abc113_c-ac.rs
@@ -0,0 +1,81 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+// -----------------------------------------------
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let (N, M) = read!(usize, usize);
+
+    let mut pref = vec![vec![]; N + 1];
+    for i in 0..M {
+        let (p, y) = read!(usize, i64);
+
+        pref[p].push((y, i));
+    }
+
+    let mut numbers = vec![];
+
+    for p in 1..N + 1 {
+        pref[p].sort();
+
+        numbers.extend(
+            pref[p]
+                .iter()
+                .enumerate()
+                .map(|(x, &(y, i))| (i, format!("{:>06}{:>06}", p, 1 + x))),
+        );
+    }
+
+    numbers.sort();
+    for (_, n) in numbers {
+        println!("{}", n)
+    }
+}
