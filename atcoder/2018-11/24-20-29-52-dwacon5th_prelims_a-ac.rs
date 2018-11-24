eaa889b4c90048ef349c0646cc37922bafeb584b
vain0x <vainzerox@gmail.com>
2018-11-24 20:29:52 +0900

    Add https://atcoder.jp/contests/dwacon5th-prelims/submissions/3655230

---
+++ atcoder/2018-11/24-20-29-52-dwacon5th_prelims_a-ac.rs
@@ -0,0 +1,72 @@
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+//
+// See the bottom of file for solution.
+// -----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let N = read!(usize);
+    let A = read![[f64]];
+
+    let mean = A.iter().fold(0.0, |n, a| n + a) / A.len() as f64;
+    let mut min_i = 0;
+    let mut min_d = std::f64::MAX;
+    for i in 0..N {
+        let d = (A[i] - mean).abs();
+        if min_d > d {
+            min_d = d;
+            min_i = i;
+        }
+    }
+
+    println!("{}", min_i);
+}
