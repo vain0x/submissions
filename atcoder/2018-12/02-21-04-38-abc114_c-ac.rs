73fc39b15b1ac24ef3dfca094e84ebdb348c8750
vain0x <vainzerox@gmail.com>
2018-12-02 21:04:38 +0900

    https://atcoder.jp/contests/abc114/submissions/3702290

---
+++ atcoder/2018-12/02-21-04-38-abc114_c-ac.rs
@@ -0,0 +1,77 @@
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+//
+// See the bottom of file for solution.
+// -----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn dfs(n: i64, b3: bool, b5: bool, b7: bool, m: i64) -> i64 {
+    if n > m {
+        return 0;
+    }
+
+    let mut k = 0_i64;
+
+    if b3 && b5 && b7 {
+        k += 1;
+    }
+
+    k += dfs(n * 10 + 3, true, b5, b7, m);
+    k += dfs(n * 10 + 5, b3, true, b7, m);
+    k += dfs(n * 10 + 7, b3, b5, true, m);
+
+    k
+}
+
+fn main() {
+    let m = read!(i64);
+    println!("{}", dfs(0, false, false, false, m));
+}
