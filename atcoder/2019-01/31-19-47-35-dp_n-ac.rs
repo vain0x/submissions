74e221b1c48c6b9c81b58bb7e3443843661fd8dc
vain0x <vainzerox@gmail.com>
2019-01-31 19:47:35 +0900

    Add https://atcoder.jp/contests/dp/submissions/4133358

---
+++ atcoder/2019-01/31-19-47-35-dp_n-ac.rs
@@ -0,0 +1,92 @@
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+//
+// See the bottom of file for solution.
+// -----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let N = read!(usize);
+    let A = read![[i64]];
+
+    let mut acc = vec![0; N + 1];
+    for i in 0..N {
+        acc[i + 1] = acc[i] + A[i];
+    }
+
+    // dp[l][r] : 区間 l..r を1個のスライムにするときにかかる最小のコスト
+    let mut dp = vec![vec![std::i64::MAX; N + 1]; N + 1];
+
+    // dp[l][l + 1] = 0
+    // dp[l][r] = dp[l][m] + dp[m][r] + acc[r] - acc[l]
+
+    for h in 1..N + 1 {
+        for l in 0..N {
+            let r = l + h;
+            if r > N {
+                continue;
+            }
+
+            if h == 1 {
+                dp[l][r] = 0;
+                continue;
+            }
+
+            for m in l + 1..r {
+                dp[l][r] = min(dp[l][r], dp[l][m] + dp[m][r] + acc[r] - acc[l])
+            }
+        }
+    }
+
+    debug!(acc, dp);
+
+    println!("{}", dp[0][N])
+}
