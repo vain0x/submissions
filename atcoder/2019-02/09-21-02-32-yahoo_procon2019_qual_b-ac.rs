d53321ba5e64899c9e676e8747b05b651bb40325
vain0x <vainzerox@gmail.com>
2019-02-09 21:02:32 +0900

    Add https://atcoder.jp/contests/yahoo-procon2019-qual/submissions/4204518

---
+++ atcoder/2019-02/09-21-02-32-yahoo_procon2019_qual_b-ac.rs
@@ -0,0 +1,77 @@
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+//
+// See the bottom of file for solution.
+// -----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let T = read!(usize, usize; 3);
+
+    let mut deg = vec![0; 4];
+    for (u, v) in T {
+        let u = u - 1;
+        let v = v - 1;
+        deg[u] += 1;
+        deg[v] += 1;
+    }
+
+    debug!(deg);
+
+    println!(
+        "{}",
+        if deg.into_iter().filter(|d| d % 2 == 1).count() <= 2 {
+            "YES"
+        } else {
+            "NO"
+        }
+    )
+}
