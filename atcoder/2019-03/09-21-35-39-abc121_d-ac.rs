a1492bcd5e3d40d233ec06ebb67bebf704bf2cae
vain0x <vainzerox@gmail.com>
2019-03-09 21:35:39 +0900

    Add https://atcoder.jp/contests/abc121/submissions/4522813

---
+++ atcoder/2019-03/09-21-35-39-abc121_d-ac.rs
@@ -0,0 +1,83 @@
+// -----------------------------------------------
+// Framework <https://github.com/vain0x/procon>
+//
+// See the bottom of file for solution.
+// -----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+// g(x) = f(0, x)
+fn g(x: i64) -> i64 {
+    let mut z = {
+        let r = x % 4;
+        (if r == 1 || r == 2 { 1 } else { 0 })
+    };
+    let mut p = 2_i64;
+    for h in 1..60 {
+        let r = x % (p * 2);
+        if r >= p && (r - p) % 2 == 0 {
+            z |= p;
+        }
+        debug!((h, p, r, (r - p) % 2));
+        p *= 2;
+    }
+    z
+}
+
+fn main() {
+    let (A, B) = read!(i64, i64);
+    println!("{}", if A == 0 { g(B) } else { g(A - 1) ^ g(B) })
+}
