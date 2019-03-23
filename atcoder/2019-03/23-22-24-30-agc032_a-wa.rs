205040374a37a587ca1b577d5e8543711e90fcde
vain0x <vainzerox@gmail.com>
2019-03-23 22:24:30 +0900

    Add https://atcoder.jp/contests/agc032/submissions/4670718

---
+++ atcoder/2019-03/23-22-24-30-agc032_a-wa.rs
@@ -0,0 +1,109 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let N = read!(usize);
+    let mut B = read![[usize]];
+
+    for i in 0..N {
+        B[i] -= 1;
+    }
+
+    let mut ok = true;
+    let mut done = vec![false; N];
+    let mut C = vec![];
+    for _ in 0..N {
+        let mut k = 0;
+        let mut last = 0;
+
+        for i in 0..N {
+            if done[i] {
+                continue;
+            }
+
+            if B[i] != k {
+                break;
+            }
+
+            k += 1;
+            last = i;
+        }
+
+        if k == 0 {
+            ok = false;
+            break;
+        }
+
+        done[last] = true;
+        C.push(k - 1);
+    }
+
+    if !ok {
+        println!("-1")
+    } else {
+        C.reverse();
+        for c in C {
+            println!("{}", c + 1)
+        }
+    }
+}
