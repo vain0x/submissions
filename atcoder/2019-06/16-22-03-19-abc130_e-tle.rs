410ce16d7e5edf45d67c9d0478f4671ed50bf6c2
vain0x <vainzerox@gmail.com>
2019-06-16 22:03:19 +0900

    Add https://atcoder.jp/contests/abc130/submissions/5976332

---
+++ atcoder/2019-06/16-22-03-19-abc130_e-tle.rs
@@ -0,0 +1,189 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn pow(x: i64, n: i64) -> i64 {
+    let (mut x, mut y, mut n) = (x % P, 1_i64, n);
+    while n > 0 {
+        if n % 2 != 0 {
+            y = (y * x) % P;
+            n -= 1;
+        }
+
+        x = (x * x) % P;
+        n /= 2;
+    }
+    y
+}
+
+/// Represents an element of finite field.
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Default)]
+struct ModInt(i64);
+
+impl ModInt {
+    fn pow(self, e: i64) -> Self {
+        pow(self.0, e).into()
+    }
+}
+
+impl Debug for ModInt {
+    fn fmt(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_fmt(format_args!("{:?}", self.0))
+    }
+}
+
+impl Display for ModInt {
+    fn fmt(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_fmt(format_args!("{}", self.0))
+    }
+}
+
+impl From<i64> for ModInt {
+    fn from(value: i64) -> Self {
+        ModInt((value % P + P) % P)
+    }
+}
+
+// Generate binary operation traits.
+macro_rules! impl_binary_op_for_modint {
+    ($op_trait:ident, $op:ident, $assign_trait:ident, $assign:ident $(, $f:ident)*) => {
+        $(impl $op_trait<ModInt> for ModInt {
+            type Output = Self;
+
+            fn $op(self, other: Self) -> Self {
+                ModInt::from((self.0).$f(other.0))
+            }
+        })*
+
+        impl $op_trait<i64> for ModInt {
+            type Output = Self;
+
+            fn $op(self, other: i64) -> Self {
+                self.$op(ModInt::from(other))
+            }
+        }
+
+        impl $assign_trait<ModInt> for ModInt {
+            fn $assign(&mut self, other: Self) {
+                *self = self.$op(other)
+            }
+        }
+
+        impl $assign_trait<i64> for ModInt {
+            fn $assign(&mut self, other: i64) {
+                *self = self.$op(other)
+            }
+        }
+    };
+}
+
+impl_binary_op_for_modint! {Add, add, AddAssign, add_assign, add}
+impl_binary_op_for_modint! {Sub, sub, SubAssign, sub_assign, sub}
+impl_binary_op_for_modint! {Mul, mul, MulAssign, mul_assign, mul}
+impl_binary_op_for_modint! {Div, div, DivAssign, div_assign}
+
+impl Div<ModInt> for ModInt {
+    type Output = ModInt;
+
+    fn div(self, other: Self) -> Self {
+        self * other.pow(P - 2)
+    }
+}
+
+const P: i64 = 1_000_000_007;
+
+fn main() {
+    let (sn, tn) = read!(usize, usize);
+    let S = read![[i64]];
+    let T = read![[i64]];
+
+    let mut inc = vec![ModInt::from(0); sn + 1];
+
+    // dp[si] = n : S[..si] の部分列と T[..ti] の部分列の組み合わせの総数
+    let mut dp = vec![ModInt::from(1); sn + 1];
+
+    for ti in 0..tn {
+        for z in 0..sn {
+            if S[z] == T[ti] {
+                inc[z] += dp[z];
+            }
+        }
+
+        for si in 0..sn {
+            dp[si + 1] = dp[si] + inc[si];
+        }
+
+        debug!(ti, inc, dp);
+    }
+
+    println!("{}", dp[sn])
+}
+
+/*
+
+4 3
+1 2 3 1
+2 1 3
+
+#=> 10
+
+*/
