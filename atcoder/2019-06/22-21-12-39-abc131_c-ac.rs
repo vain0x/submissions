a6a794abfcc0aa4ca0ab5e377a7af484420597f9
vain0x <vainzerox@gmail.com>
2019-06-22 21:12:39 +0900

    Add https://atcoder.jp/contests/abc131/submissions/6058915

---
+++ atcoder/2019-06/22-21-12-39-abc131_c-ac.rs
@@ -0,0 +1,89 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn gcd(x: i64, y: i64) -> i64 {
+    if y == 0 {
+        x.abs()
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn anti_division_count(X: i64, C: i64, D: i64) -> i64 {
+    let mut n = 0;
+    n += X / C;
+    n += X / D;
+    n -= X / (C / gcd(C, D) * D);
+
+    X - n
+}
+
+fn main() {
+    let (A, B, C, D) = read!(i64, i64, i64, i64);
+
+    let b = anti_division_count(B, C, D);
+    let a = anti_division_count(A - 1, C, D);
+    debug!(b, a);
+
+    println!("{}", b - a)
+}
