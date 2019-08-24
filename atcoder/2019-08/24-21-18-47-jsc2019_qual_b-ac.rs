45ead0316ca570978fce495b95327bfbe5558f2d
vain0x <vainzerox@gmail.com>
2019-08-24 21:18:47 +0900

    Add https://atcoder.jp/contests/jsc2019-qual/submissions/7111261

---
+++ atcoder/2019-08/24-21-18-47-jsc2019_qual_b-ac.rs
@@ -0,0 +1,105 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+const P: i64 = 1_000_000_007;
+
+pub fn pow(x: i64, n: i64) -> i64 {
+    let (mut x, mut y, mut n) = (x % P, 1_i64, n);
+    while n > 0 {
+        if n % 2 != 0 {
+            y = (y * x) % P;
+            n -= 1;
+        }
+
+        x = (x * x) % P;
+        n /= 2;
+    }
+    y
+}
+
+fn main() {
+    let (_, K) = read!(usize, i64);
+    let A = read![[i64]];
+    let N = A.len();
+
+    let mut u = 0_i64;
+    let mut pair = 0_i64;
+    for i in 0..N {
+        for j in i + 1..N {
+            if A[i] > A[j] {
+                u += 1;
+            }
+        }
+
+        for j in 0..N {
+            if A[j] > A[i] {
+                pair += 1;
+            }
+        }
+    }
+    debug!(u, pair, K * (K - 1) / 2);
+
+    let v = (K * (K - 1) / 2) % P * pair % P;
+
+    println!("{}", (u * K % P + v) % P)
+}
