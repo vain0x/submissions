2d4d09c4954367b1c83660f64d34f88bf5f43195
vain0x <vainzerox@gmail.com>
2019-09-28 21:08:16 +0900

    Add https://atcoder.jp/contests/abc142/submissions/7742560

---
+++ atcoder/2019-09/28-21-08-16-abc142_d-ac.rs
@@ -0,0 +1,99 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn gcd(x: i64, y: i64) -> i64 {
+    if y == 0 {
+        x.abs()
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn f(mut x: i64) -> BTreeMap<i64, i64> {
+    let mut ms = BTreeMap::new();
+    let r = (x as f64).sqrt() as i64 + 1;
+
+    for p in 2..r as i64 {
+        let mut m = 0;
+
+        while x >= p && x % p == 0 {
+            x /= p;
+            m += 1;
+        }
+
+        if m > 0 {
+            ms.insert(p, m);
+        }
+    }
+
+    if x > 1 {
+        ms.insert(x, 1);
+    }
+
+    ms
+}
+
+fn main() {
+    let (A, B) = read!(i64, i64);
+    println!("{}", f(gcd(A, B)).len() + 1)
+}
