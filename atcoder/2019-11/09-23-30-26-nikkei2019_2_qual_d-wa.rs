a33d83a46999c7c49fa95480f6edad04930ab122
vain0x <vainzerox@gmail.com>
2019-11-09 23:30:26 +0900

    Add https://atcoder.jp/contests/nikkei2019-2-qual/submissions/8371341

---
+++ atcoder/2019-11/09-23-30-26-nikkei2019_2_qual_d-wa.rs
@@ -0,0 +1,125 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+fn main() {
+    let (N, M) = read!(usize, usize);
+    let T = read![usize, usize, i64; M];
+
+    let mut b = vec![];
+    for &(l, r, _) in &T {
+        b.push(l);
+        b.push(r);
+    }
+    b.push(N);
+    b.sort();
+    b.dedup();
+
+    let mut G = vec![vec![]; N + 1];
+    for &(l, r, c) in &T {
+        G[l].push((r, c));
+    }
+
+    for w in b.windows(2) {
+        G[w[1]].push((w[0], 0));
+    }
+
+    // dyijkstra
+    let mut dist = vec![std::i64::MAX; N + 1];
+    let mut q: BinaryHeap<(Rev<i64>, usize)> = BinaryHeap::new();
+
+    dist[1] = 0;
+    q.push((Rev(0), 1));
+    while let Some((Rev(d), u)) = q.pop() {
+        for i in 0..G[u].len() {
+            let (v, c) = G[u][i];
+            if d + c < dist[v] {
+                dist[v] = d + c;
+                if v == N {
+                    break;
+                }
+                q.push((Rev(d + c), v));
+            }
+        }
+    }
+
+    if dist[N] == std::i64::MAX {
+        println!("-1");
+    } else {
+        println!("{}", dist[N])
+    }
+}
