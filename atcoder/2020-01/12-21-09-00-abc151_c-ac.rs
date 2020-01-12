87050efbd6cef8ae48524226986b795327e9265d
vain0x <vainzerox@gmail.com>
2020-01-12 21:09:00 +0900

    Add https://atcoder.jp/contests/abc151/submissions/9448397

---
+++ atcoder/2020-01/12-21-09-00-abc151_c-ac.rs
@@ -0,0 +1,109 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+/*
+2 5
+1 WA
+1 WA
+2 WA
+2 AC
+2 WA
+=>
+1 1
+*/
+
+fn main() {
+    let (N, M) = read!(usize, usize);
+
+    let mut ac = vec![false; N];
+    let mut penalty = vec![0; N];
+
+    for (mut p, s) in read![usize, String; M] {
+        p -= 1;
+
+        if ac[p] {
+            continue;
+        }
+
+        match s.as_str() {
+            "AC" => {
+                ac[p] = true;
+            }
+            "WA" => {
+                penalty[p] += 1;
+            }
+            _ => unreachable!("invalid input"),
+        }
+    }
+
+    let mut s = 0;
+    let mut t = 0;
+    for i in 0..N {
+        if ac[i] {
+            s += 1;
+            t += penalty[i];
+        }
+    }
+
+    println!("{} {}", s, t)
+}
