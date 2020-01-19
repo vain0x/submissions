39af65656c578209b2f8707faac5bfdaa05c6736
vain0x <vainzerox@gmail.com>
2020-01-19 21:32:18 +0900

    Add https://atcoder.jp/contests/abc152/submissions/9617788

---
+++ atcoder/2020-01/19-21-32-18-abc152_d-ac.rs
@@ -0,0 +1,95 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn msd(mut n: i64) -> i64 {
+    while n >= 10 {
+        n /= 10;
+    }
+    n
+}
+
+fn main() {
+    let N = read!(i64);
+
+    let mut map = vec![vec![0_i64; 10]; 10];
+    let mut count = 0_i64;
+
+    for A in 1..N + 1 {
+        let x = msd(A) as usize;
+        let y = (A % 10) as usize;
+
+        if x == 0 || y == 0 {
+            continue;
+        }
+
+        count += if x == y {
+            map[y][x] * 2 + 1
+        } else {
+            map[y][x] * 2
+        };
+
+        map[x][y] += 1;
+    }
+
+    println!("{}", count)
+}
