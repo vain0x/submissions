a7af4fa807b66318ca25371912c60c51d4b96e91
vain0x <vainzerox@gmail.com>
2020-02-09 21:15:12 +0900

    Add https://atcoder.jp/contests/abc154/submissions/9986685

---
+++ atcoder/2020-02/09-21-15-12-abc154_d-ac.rs
@@ -0,0 +1,99 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn f(p: i64) -> f64 {
+    ((p + 1) as f64) / 2.0
+}
+
+fn main() {
+    let (N, K) = read!(usize, usize);
+    let P = read![[i64]];
+
+    let mut current = P[0..K].iter().cloned().map(f).sum::<f64>();
+
+    let mut i = K;
+    let mut m = 0.0;
+    while i <= N {
+        if m < current {
+            m = current;
+        }
+
+        if i == N {
+            break;
+        }
+
+        {
+            let p = P[i - K];
+            current -= f(p);
+        }
+
+        {
+            let p = P[i];
+            current += f(p);
+        }
+
+        i += 1;
+    }
+
+    println!("{}", m)
+}
