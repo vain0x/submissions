0a832a7d10eeeaeb32cf078033772ff286ff9894
vain0x <vainzerox@gmail.com>
2020-04-05 21:29:55 +0900

    Add https://atcoder.jp/contests/judge-update-202004/submissions/11587089

---
+++ atcoder/2020-04/05-21-29-55-judge_update_202004_d-ac.rs
@@ -0,0 +1,107 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+pub fn gcd(x: i64, y: i64) -> i64 {
+    if y == 0 {
+        x.abs()
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn main() {
+    let (N, _) = read!(usize, usize);
+    let A = read![[i64]];
+    let S = read![[i64]];
+
+    let mut acc = A.clone();
+    for i in 1..N {
+        acc[i] = gcd(acc[i], acc[i - 1]);
+    }
+    debug!(acc);
+
+    for s in S {
+        let t = {
+            let X = gcd(s, acc[N - 1]);
+            if X != 1 {
+                X
+            } else {
+                let mut l = -1_isize;
+                let mut r = (N - 1) as isize;
+
+                while r - l > 1 {
+                    let m = (l + r) / 2;
+                    if gcd(s, acc[m as usize]) == 1 {
+                        r = m;
+                    } else {
+                        l = m;
+                    }
+                }
+
+                r as i64 + 1
+            }
+        };
+
+        println!("{}", t)
+    }
+}
