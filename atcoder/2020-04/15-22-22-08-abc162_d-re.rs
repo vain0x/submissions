6f4dafc1847ec96bf04e355d130809b4f3c7f594
vain0x <vainzerox@gmail.com>
2020-04-15 22:22:08 +0900

    Add https://atcoder.jp/contests/abc162/submissions/11957062

---
+++ atcoder/2020-04/15-22-22-08-abc162_d-re.rs
@@ -0,0 +1,54 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let N = scan.word::<usize>();
+    let S = scan.word::<String>().chars().collect::<Vec<_>>();
+
+    let mut freq = HashMap::new();
+    for &c in &S {
+        *freq.entry(c).or_insert(0) += 1_usize;
+    }
+
+    let mut count = freq[&'R'] * freq[&'G'] * freq[&'B'];
+
+    for i in 0..N {
+        for d in 0..N {
+            let j = i + d;
+            let k = j + d;
+
+            if j < N && k < N && S[i] != S[j] && S[i] != S[k] && S[j] != S[k] {
+                count -= 1;
+            }
+        }
+    }
+
+    println!("{}", count);
+}
