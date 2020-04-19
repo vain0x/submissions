19bbc23739b77a9de5398ebd347d0d850728bfab
vain0x <vainzerox@gmail.com>
2020-04-19 21:08:24 +0900

    Add https://atcoder.jp/contests/abc163/submissions/12096931

---
+++ atcoder/2020-04/19-21-08-24-abc163_b-ac.rs
@@ -0,0 +1,43 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let N = scan.word::<i64>();
+    let M = scan.word::<usize>();
+    let A = scan.list::<i64>(M);
+
+    let sum = A.iter().sum::<i64>();
+
+    if sum <= N {
+        println!("{}", N - sum);
+    } else {
+        println!("-1");
+    }
+}
