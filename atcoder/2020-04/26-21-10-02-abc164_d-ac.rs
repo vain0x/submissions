c32349ad71823013a1cbb2dad72ab9ba636be2f4
vain0x <vainzerox@gmail.com>
2020-04-26 21:10:02 +0900

    Add https://atcoder.jp/contests/abc164/submissions/12355837

---
+++ atcoder/2020-04/26-21-10-02-abc164_d-ac.rs
@@ -0,0 +1,62 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn ch(c: char) -> i64 {
+    (c as u8 - b'0') as i64
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let S = scan.word::<String>().chars().collect::<Vec<_>>();
+
+    let N = S.len();
+    const P: i64 = 2019;
+
+    let mut T = vec![0; N + 1];
+    let mut h = 1;
+    for r in (0..N).rev() {
+        let d = ch(S[r]);
+        T[r] = (d * h + T[r + 1]) % P;
+        h = (h * 10) % P;
+    }
+
+    let mut freq = vec![0_usize; P as usize];
+    for i in 0..T.len() {
+        freq[T[i] as usize] += 1;
+    }
+
+    let mut total = 0_usize;
+    for i in 0..freq.len() {
+        if freq[i] >= 1 {
+            total += freq[i] * (freq[i] - 1) / 2;
+        }
+    }
+
+    println!("{}", total);
+}
