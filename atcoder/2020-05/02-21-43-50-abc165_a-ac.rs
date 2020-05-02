e2fdb86cc6916b495ea79053561b82c0300dd47a
vain0x <vainzerox@gmail.com>
2020-05-02 21:43:50 +0900

    Add https://atcoder.jp/contests/abc165/submissions/12610194

---
+++ atcoder/2020-05/02-21-43-50-abc165_a-ac.rs
@@ -0,0 +1,38 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let K = scan.word::<usize>();
+    let A = scan.word::<usize>();
+    let B = scan.word::<usize>();
+    let ok = (A..=B).any(|x| x % K == 0);
+
+    println!("{}", if ok { "OK" } else { "NG" });
+}
