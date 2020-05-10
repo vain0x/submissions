2d879a852b67100381bc0b5f7dc216eaef14c1c8
vain0x <vainzerox@gmail.com>
2020-05-10 21:09:59 +0900

    Add https://atcoder.jp/contests/abc167/submissions/13037497

---
+++ atcoder/2020-05/10-21-09-59-abc167_b-ac.rs
@@ -0,0 +1,49 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let A = scan.word::<usize>();
+    let B = scan.word::<usize>();
+    let C = scan.word::<usize>();
+    let mut K = scan.word::<usize>();
+
+    let mut m = 0_i64;
+    let a = A.min(K);
+    m += a as i64;
+    K -= a;
+
+    let b = B.min(K);
+    K -= b;
+
+    let c = C.min(K);
+    m -= c as i64;
+
+    println!("{}", m);
+}
