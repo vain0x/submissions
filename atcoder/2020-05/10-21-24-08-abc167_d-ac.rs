4575772ef69913b4f37e801d4d419f2de6e11dbe
vain0x <vainzerox@gmail.com>
2020-05-10 21:24:08 +0900

    Add https://atcoder.jp/contests/abc167/submissions/13049967

---
+++ atcoder/2020-05/10-21-24-08-abc167_d-ac.rs
@@ -0,0 +1,66 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let N = scan.word::<usize>();
+    let mut K = scan.word::<usize>();
+    let A = scan.list::<usize>(N);
+
+    let mut x = 0;
+    let mut step = vec![N; N];
+    let mut t = 0;
+    let e;
+
+    loop {
+        if step[x] != N {
+            e = x;
+            t -= step[x];
+            break;
+        }
+
+        step[x] = t;
+        x = A[x] - 1;
+        t += 1;
+    }
+
+    if K >= step[e] {
+        K -= step[e];
+        K %= t;
+        x = e;
+    } else {
+        x = 0;
+    }
+
+    for _ in 0..K {
+        x = A[x] - 1;
+    }
+
+    println!("{}", x + 1);
+}
