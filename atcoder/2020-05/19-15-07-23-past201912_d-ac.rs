701de1951a8077b6be84f0b79b440e1d70a1039a
vain0x <vainzerox@gmail.com>
2020-05-19 15:07:23 +0900

    Add https://atcoder.jp/contests/past201912-open/submissions/13396581

---
+++ atcoder/2020-05/19-15-07-23-past201912_d-ac.rs
@@ -0,0 +1,67 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let N = read!(usize);
+    let A = read![usize; N];
+
+    let mut set = BTreeSet::new();
+    let mut dup = None;
+    for a in A {
+        if !set.insert(a) {
+            dup = Some(a);
+        }
+    }
+    let missed = (1..N + 1).find(|i| !set.contains(i));
+
+    match (missed, dup) {
+        (Some(x), Some(y)) => println!("{} {}", y, x),
+        _ => println!("Correct"),
+    }
+}
