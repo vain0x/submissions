11b9d83ac2d5570d0000b5effa0d7559b9223cdd
vain0x <vainzerox@gmail.com>
2020-05-26 21:55:33 +0900

    Add https://atcoder.jp/contests/abc138/submissions/13621870

---
+++ atcoder/2020-05/26-21-55-33-abc138_e-ac.rs
@@ -0,0 +1,112 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+const INF: usize = 1 << 60;
+
+fn char_to_index(c: char) -> usize {
+    (c as u8 - b'a') as usize
+}
+
+fn main() {
+    let s = rl().chars().collect::<Vec<_>>();
+    let t = rl();
+
+    let sn = s.len();
+
+    let mut skip = vec![vec![INF; 26]; sn];
+    let mut last = vec![INF; 26];
+
+    let mut ai = sn * 2;
+    for _ in 0..2 {
+        for i in (0..sn).rev() {
+            ai -= 1;
+
+            let ci = char_to_index(s[i]);
+            last[ci] = ai;
+
+            for ci in 0..26 {
+                skip[i][ci] = last[ci];
+            }
+        }
+    }
+
+    let mut i = 0_usize;
+    let mut ok = true;
+
+    for c in t.chars() {
+        let ci = char_to_index(c);
+        let k = skip[i % sn][ci];
+        if k == INF {
+            ok = false;
+            break;
+        }
+
+        i += k - (i % sn) + 1;
+    }
+
+    if ok {
+        println!("{}", i);
+    } else {
+        println!("-1");
+    }
+}
