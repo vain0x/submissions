2fea2776b36b7d1bd2e7b7f84b195887a6d7ff0e
vain0x <vainzerox@gmail.com>
2020-05-30 21:01:16 +0900

    Add https://atcoder.jp/contests/nomura2020/submissions/13714639

---
+++ atcoder/2020-05/30-21-01-16-nomura2020_a-ac.rs
@@ -0,0 +1,41 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let H1 = scan.word::<usize>();
+    let M1 = scan.word::<usize>();
+    let H2 = scan.word::<usize>();
+    let M2 = scan.word::<usize>();
+    let K = scan.word::<usize>();
+
+    let t = (H2 * 60 + M2) - (H1 * 60 + M1) - K;
+
+    println!("{}", t);
+}
