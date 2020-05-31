11133fb57050a541e77695b79d5a2de3f930fc21
vain0x <vainzerox@gmail.com>
2020-05-31 21:16:38 +0900

    Add https://atcoder.jp/contests/abc169/submissions/13810219

---
+++ atcoder/2020-05/31-21-16-38-abc169_d-ac.rs
@@ -0,0 +1,84 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+pub fn factorize(mut x: i64) -> BTreeMap<i64, i64> {
+    let mut ms = BTreeMap::new();
+    let r = (x as f64).sqrt() as i64 + 1;
+
+    for p in 2..r as i64 {
+        let mut m = 0;
+
+        while x >= p && x % p == 0 {
+            x /= p;
+            m += 1;
+        }
+
+        if m > 0 {
+            ms.insert(p, m);
+        }
+    }
+
+    // `x` can have a prime factor larger than √x at most one.
+    if x > 1 {
+        ms.insert(x, 1);
+    }
+
+    ms
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let mut N = scan.word::<i64>();
+    let mut q = vec![];
+
+    for (p, m) in factorize(N) {
+        let mut t = p;
+        for _ in 1..=m {
+            q.push(t);
+            t *= p;
+        }
+    }
+
+    q.sort();
+    let mut count = 0;
+
+    for z in q.drain(..) {
+        if z > N {
+            break;
+        }
+        if N % z != 0 {
+            continue;
+        }
+
+        N /= z;
+        count += 1;
+    }
+
+    println!("{}", count);
+}
