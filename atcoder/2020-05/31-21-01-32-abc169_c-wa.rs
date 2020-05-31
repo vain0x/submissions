d7f8216ed9254b844354dc1263bdd0fc637219e4
vain0x <vainzerox@gmail.com>
2020-05-31 21:01:32 +0900

    Add https://atcoder.jp/contests/abc169/submissions/13780937

---
+++ atcoder/2020-05/31-21-01-32-abc169_c-wa.rs
@@ -0,0 +1,36 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let A = scan.word::<i64>();
+    let B = scan.word::<f64>();
+
+    println!("{}", (A * (B * 100.0) as i64) / 100);
+}
