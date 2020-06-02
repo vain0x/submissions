d0ed0bf0f087c87a9486583c67dcfbf9f3484b81
vain0x <vainzerox@gmail.com>
2020-06-02 22:01:48 +0900

    Add https://atcoder.jp/contests/abc158/submissions/13955956

---
+++ atcoder/2020-06/02-22-01-48-abc158_e-ac.rs
@@ -0,0 +1,100 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let (_, P) = read!(usize, usize);
+    let S = rl();
+
+    let mut total = 0;
+
+    if P == 2 || P == 5 {
+        for (i, c) in S.chars().enumerate() {
+            if (c as u8 - b'0') as usize % P == 0 {
+                total += i + 1;
+            }
+        }
+    } else {
+        let mut freq = vec![0; P];
+        let mut x = 0;
+        let mut h = 1;
+
+        for c in S.chars().rev() {
+            x += (c as u8 - b'0') as usize * h;
+            x %= P;
+            h *= 10;
+            h %= P;
+            freq[x] += 1_usize;
+        }
+        debug!(freq);
+
+        freq[0] += 1_usize;
+
+        for &m in &freq {
+            if m >= 1 {
+                total += m * (m - 1) / 2;
+            }
+        }
+    }
+
+    println!("{}", total)
+}
