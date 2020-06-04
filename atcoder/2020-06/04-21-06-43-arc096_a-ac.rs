878a5e597a5a235c67d6f63877052a8aedc61b7d
vain0x <vainzerox@gmail.com>
2020-06-04 21:06:43 +0900

    Add https://atcoder.jp/contests/abc095/submissions/14004386

---
+++ atcoder/2020-06/04-21-06-43-arc096_a-ac.rs
@@ -0,0 +1,74 @@
+//! ----------------------------------------------
+//! Framework <https://github.com/vain0x/procon>
+//!
+//! See the bottom of file for solution.
+//! ----------------------------------------------
+
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use std::cell::RefCell;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::fmt::{Debug, Display, Formatter, Write as FmtWrite};
+use std::io::{stderr, stdin, BufRead, Write};
+use std::mem::{replace, swap};
+use std::ops::*;
+use std::rc::Rc;
+
+/// Print values to standard error if debug mode.
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($e:expr),*) => {
+        #[cfg(debug_assertions)]
+        $({
+            let (e, mut err) = (stringify!($e), stderr());
+            writeln!(err, "\x1B[33m{}\x1B[0m = {:?}", e, $e).unwrap()
+        })*
+    };
+}
+
+/// Read from standard input and parse each word.
+/// - `read!(T, U, ..)` parses a line as a tuple of words.
+/// - `read![[T]]` parses a line as an array of words.
+/// - `read![..; N]` parses `N` lines, using `read!(..)` repeatedly.
+#[allow(unused_macros)]
+macro_rules! read {
+    ([$t:ty] ; $n:expr) =>
+        ((0..$n).map(|_| read!([$t])).collect::<Vec<_>>());
+    ($($t:ty),+ ; $n:expr) =>
+        ((0..$n).map(|_| read!($($t),+)).collect::<Vec<_>>());
+    ([$t:ty]) =>
+        (rl().split_whitespace().map(|w| w.parse().unwrap()).collect::<Vec<$t>>());
+    ($($t:ty),*) => {{
+        let buf = rl();
+        let mut w = buf.split_whitespace();
+        ($(w.next().unwrap().parse::<$t>().unwrap()),*)
+    }};
+}
+
+/// Read a line from standard input.
+#[allow(dead_code)]
+fn rl() -> String {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    #[allow(deprecated)]
+    buf.trim_right().to_owned()
+}
+
+// -----------------------------------------------
+// Solution
+// -----------------------------------------------
+
+fn main() {
+    let (A, B, C, X, Y) = read!(i64, i64, i64, i64, i64);
+
+    let mut cost = std::i64::MAX;
+    for i in 0..max(X, Y) + 1 {
+        let (x, y) = (max(X - i, 0), max(Y - i, 0));
+        cost = min(cost, x * A + y * B + i * C * 2);
+    }
+
+    println!("{}", cost)
+}
