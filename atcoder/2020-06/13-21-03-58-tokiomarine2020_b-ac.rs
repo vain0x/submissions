a86180c0c79c75fac79a68e4c2711def9a905151
vain0x <vainzerox@gmail.com>
2020-06-13 21:03:58 +0900

    Add https://atcoder.jp/contests/tokiomarine2020/submissions/14223247

---
+++ atcoder/2020-06/13-21-03-58-tokiomarine2020_b-ac.rs
@@ -0,0 +1,41 @@
+//! Framework <https://github.com/vain0x/procon>
+
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use std::collections::*;
+
+pub struct Scan(Box<dyn Iterator<Item = &'static str>>); // '
+
+impl Scan {
+    fn new() -> Self {
+        let mut buf = String::new();
+        let read_line = move || {
+            std::io::stdin().read_line(&mut buf).unwrap();
+            Box::leak(buf.split_off(0).into_boxed_str()).split_whitespace()
+        };
+        Scan(Box::new(std::iter::repeat_with(read_line).flatten()))
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        std::iter::repeat_with(|| self.word()).take(len).collect()
+    }
+}
+
+fn main() {
+    let mut scan = Scan::new();
+
+    let A = scan.word::<i64>();
+    let V = scan.word::<i64>();
+    let B = scan.word::<i64>();
+    let W = scan.word::<i64>();
+    let T = scan.word::<i64>();
+
+    let ok = V >= W && (V - W) * T >= (A - B).abs();
+
+    println!("{}", if ok { "YES" } else { "NO" });
+}
